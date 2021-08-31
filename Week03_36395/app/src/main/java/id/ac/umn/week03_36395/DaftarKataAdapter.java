dabc7900dfd9389fbae4b66cd180f31ffe756c09
Julius <juliuscannavaro11@gmail.com>
2021-08-31 12:03:46 +0700

    Modul3

---
+++ Week03_36395/app/src/main/java/id/ac/umn/week03_36395/DaftarKataAdapter.java
@@ -0,0 +1,63 @@
+package id.ac.umn.week03_36395;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class DaftarKataAdapter extends RecyclerView.Adapter<DaftarKataAdapter.KataViewHolder> {
+    private final LinkedList<String> mDaftarKata;
+    private LayoutInflater mInflater;
+
+    public DaftarKataAdapter(Context context, LinkedList<String> daftarKata) {
+        mInflater = LayoutInflater.from(context);
+        mDaftarKata = daftarKata;
+    }
+
+    @NonNull
+    @Override
+    public DaftarKataAdapter.KataViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.daftarkata_item, parent, false);
+        return new KataViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull KataViewHolder holder, int position) {
+        String mCurrent = mDaftarKata.get(position);
+        holder.kataItemView.setText((mCurrent));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDaftarKata.size();
+    }
+
+    public class KataViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public final TextView kataItemView;
+        final DaftarKataAdapter mAdapter;
+
+        public KataViewHolder(@NonNull View itemView, DaftarKataAdapter adapter) {
+            super(itemView);
+            kataItemView = itemView.findViewById(R.id.kata);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int mPosition = getLayoutPosition();
+            String element = mDaftarKata.get(mPosition);
+            mDaftarKata.set(mPosition, element + " pernah diklik ");
+            mAdapter.notifyDataSetChanged();
+            Toast.makeText(view.getContext(), element + " dipilih", Toast.LENGTH_LONG).show();
+        }
+    }
+}
